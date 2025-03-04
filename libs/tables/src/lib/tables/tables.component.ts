e9220322fed17dbf602050a5e715f0179492ba89
Steven Barnhurst <stevenbarnhurst@gmail.com>
2025-03-04 11:47:20 +0100

    Adds tables lib

---
+++ libs/tables/src/lib/tables/tables.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-tables',
+  imports: [CommonModule],
+  templateUrl: './tables.component.html',
+  styleUrl: './tables.component.css',
+})
+export class TablesComponent {}
