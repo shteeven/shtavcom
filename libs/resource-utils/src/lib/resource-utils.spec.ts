f76d292070f7e84e6d1abd3dc7597d139ed17640
Steven Barnhurst <s.barnhurst@xeotek.com>
2025-03-04 12:03:38 +0100

    Adds resource-utils lib

---
+++ libs/resource-utils/src/lib/resource-utils.spec.ts
@@ -0,0 +1,7 @@
+import { resourceUtils } from './resource-utils';
+
+describe('resourceUtils', () => {
+  it('should work', () => {
+    expect(resourceUtils()).toEqual('resource-utils');
+  });
+});
