b9ef8690fad15cd67f13adccf1ebf1c664da478f
Steven Barnhurst <stevenbarnhurst@gmail.com>
2025-04-10 13:00:09 +0200

    Adds flashcards component

--- apps/sprich-mit-mir/src/app/app.component.ts
+++ apps/sprich-mit-mir/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FlashcardsComponent } from './flashcards/flashcards.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, FlashcardsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ apps/sprich-mit-mir/src/app/flashcards/flashcards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-flashcards',
+  imports: [CommonModule],
+  templateUrl: './flashcards.component.html',
+  styleUrl: './flashcards.component.css',
+})
+export class FlashcardsComponent {}
