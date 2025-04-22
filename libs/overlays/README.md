d9db262de9d854e5a517320f52d4f3a8f0db927d
Steven Barnhurst <stevenbarnhurst@gmail.com>
2025-04-22 20:57:31 +0200

    Adds overlays lib scaffolding

---
+++ libs/overlays/README.md
@@ -0,0 +1,7 @@
+# overlays
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test overlays` to execute the unit tests.
---
+++ libs/overlays/src/lib/overlays/overlays.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-overlays',
+  imports: [CommonModule],
+  templateUrl: './overlays.component.html',
+  styleUrl: './overlays.component.css',
+})
+export class OverlaysComponent {}
